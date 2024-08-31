8889f04f18380992ab4330cd521d10709413b2d1
paullarasales <ahmadzufukli@gmail.com>
2024-08-31 15:38:44 +0800

    done with the string

--- Python_Basics/day01.py
+++ Python_Basics/day01.py
@@ -1,6 +1,31 @@
-first_name = "Paul"
-last_name = "Sales"
+#String
+name = "Paul"
 
-full_name = first_name + " " + last_name
+print(name)
 
-print(full_name)
+#Lists is an ordered collection of items
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4, 5]
+
+fruits.append("orange")
+print(fruits)
+fruits.remove("banana")
+print(fruits)
+
+#Dictionaries is collection of key-value-pairs
+person = {"name": "Paul", "age": 22, "city": "San Carlos"}
+print(person["name"]);
+person["email"] = "paul@gmail.com"
+print(person)
+person.pop("age")
+print(person)
+
+#Tuples is similar to list but is immutable
+point = (10, 20)
+coordinates = (1.5, 2.5, 3.5);
+
+#Summary
+#Strings: Text Data, immutable.
+#Lists: Ordered, mutable collection of items.
+#Dictionaries: Key-value-pairs, mutable.
+#Tuples: Ordered, immutable collection of items.
