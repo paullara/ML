690654a8d5ad41962d7505304c8b402f4e4b69e4
paullarasales <ahmadzufukli@gmail.com>
2024-09-01 10:34:25 +0800

    doing some decorator exercise

--- Python_Basics/function.py
+++ Python_Basics/function.py
@@ -1,3 +1,7 @@
+import time
+import math
+import random
+from datetime import datetime
 
 def main():
     numbers = [1, 2, 3, 4, 5]
@@ -63,6 +67,45 @@ def main():
         print("This is inside the fucntion!!")
 
     function_to_be_used()
+
+    def calculate_time(func):
+        def inner1(*args, **kwargs):
+            begin = time.time()
+
+            func(*args, **kwargs)
+
+            end = time.time()
+            print("Total time taken in : ", func.__name__, end - begin)
+        return inner1
+
+    @calculate_time
+    def factorial(num):
+        time.sleep(2)
+        print(math.factorial(num))
+    factorial(10)
+
+    def log_execution_time(func):
+
+        def wrapper(*args, **kwargs):
+            start_time = datetime.now()
+            print(f"Starting function '{func.__name__}' at {start_time}")
+
+            result = func(*args, **kwargs)
+
+            end_time = datetime.now()
+            execution_time = (end_time - start_time).total_seconds()
+            print(f"Ending function '{func.__name__}' at {end_time}")
+            print(f"Function '{func.__name__}' executed in {execution_time:6f} seconds")
+            return result
+        return wrapper
+
+    @log_execution_time
+    def my_task():
+        sleep_time = random.uniform(1, 3)
+        time.sleep(sleep_time)
+        print(f"Task completed after sleeping for {sleep_time:.2f} seconds")
+
+    my_task()
     
 if __name__ == "__main__":
     main()
